twitter/util

Add API docs URL to SBT config for POM
Problem

Since Scala 2.10.2 and SBT 0.13 it's been possible to allow clients of a library
to automatically link against API documentation via some extra metadata in the
published POM. We're not currently providing this metadata for Finagle or Util.

Solution

Add the appropriate settings to the SBT builds for Util and Finagle.

Result

Projects that depend on Util or Finagle can have references in their API docs
automatically linked to ours.

RB_ID=575544

project/Build.scala
@@ -66,6 +66,8 @@ object Util extends Build {
     publishArtifact in Test := false,
     pomIncludeRepository := { _ => false },
     publishMavenStyle := true,
+    autoAPIMappings := true,
+    apiURL := Some(url("https://twitter.github.io/util/docs/")),
     pomExtra := (
       <url>https://github.com/twitter/util</url>
       <licenses>

jbripley/finagle

project/Build.scala
@@ -101,6 +101,8 @@ object Finagle extends Build {
     publishArtifact in Test := false,
     pomIncludeRepository := { _ => false },
     publishMavenStyle := true,
+    autoAPIMappings := true,
+    apiURL := Some(url("https://twitter.github.io/finagle/docs/")),
     pomExtra := (
       <url>https://github.com/twitter/finagle</url>
       <licenses>