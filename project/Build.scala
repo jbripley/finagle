twitter/scrooge

csl: Use snapshot versions in sbt while off master
Problem

It's a hassle to develop off of the `develop` branch in OSS, because
a given `develop` may depend on an unpublished `develop` in another
repository.

Solution

Although a long-term solution may entail getting [dbuild][0] working,
that's not yet satisfactory for internal twitter usage, since dbuild
assumes that there is one repository per [sbt project][1].

An interim solution is to check whether we're on the master branch,
and use a "-SNAPSHOT" suffix otherwise.

Result

If you want to develop on finagle, but depend on new changes in
util, checkout util/develop, `./sbt publish-local`, and then checkout
finagle/develop and just `./sbt compile`.  It should "just work".

[0]: http://typesafehub.github.io/dbuild
[1]: https://github.com/typesafehub/dbuild/issues/159

RB_ID=569590

project/Build.scala
@@ -10,9 +10,12 @@ import AssemblyKeys._
 import sbtbuildinfo.Plugin._
 
 object Scrooge extends Build {
-  val libVersion = "3.17.0"
-  val utilVersion = "6.23.0"
-  val finagleVersion = "6.24.0"
+  val branch = Process("git" :: "symbolic-ref" :: "--short" :: "HEAD" :: Nil).!!.trim
+  val suffix = if (branch == "master") "" else "-SNAPSHOT"
+
+  val libVersion = "3.17.0" + suffix
+  val utilVersion = "6.23.0" + suffix
+  val finagleVersion = "6.24.0" + suffix
 
   def util(which: String) = "com.twitter" %% ("util-"+which) % utilVersion
   def finagle(which: String) = "com.twitter" %% ("finagle-"+which) % finagleVersion

jbripley/finagle

project/Build.scala
@@ -6,9 +6,12 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Finagle extends Build {
-  val libVersion = "6.24.0"
+  val branch = Process("git" :: "symbolic-ref" :: "--short" :: "HEAD" :: Nil).!!.trim
+  val suffix = if (branch == "master") "" else "-SNAPSHOT"
+
+  val libVersion = "6.24.0" + suffix
   val zkVersion = "3.3.4"
-  val utilVersion = "6.23.0"
+  val utilVersion = "6.23.0" + suffix
   val ostrichVersion = "9.7.0"
   val nettyLib = "io.netty" % "netty" % "3.10.0.Final"
   val ostrichLib = "com.twitter" %% "ostrich" % ostrichVersion